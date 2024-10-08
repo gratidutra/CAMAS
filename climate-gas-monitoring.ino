c3207189913f295f80c91ae0353d18bbf4dff80e
gratidutra <gratirodrigues.gdr@gmail.com>
2024-10-08 08:34:22 -0300

    update code

--- climate-gas-monitoring.ino
+++ climate-gas-monitoring.ino
@@ -1,36 +1,70 @@
-
-#include <DHT.h>; //INCLUSÃO DE BIBLIOTECA
- 
+// PINOUT CS EM 4 ,sd card (sck 13,miso12,mosi11cs4) A1 SENSOR AMINIA SENSOR TEMOPERATURA PINO 7
+ #include <SD.h>
+ #include <DHT.h>; //INCLUSÃO DE BIBLIOTECA 
+ #include <Wire.h> 
+ const int intervalo =180000;
 #define DHTPIN 7 //PINO DIGITAL UTILIZADO PELO DHT22
 #define DHTTYPE DHT22 //DEFINE O MODELO DO SENSOR (DHT22 / AM2302)
- 
-DHT dht(DHTPIN, DHTTYPE); //PASSA OS PARÂMETROS PARA A FUNÇÃO
-  int amonia=A3; 
- int am;  
- int k = 5000;
- float amoniaconvert; 
- int y=300000;
-void setup(){
-  Serial.begin(9600); //INICIALIZA A SERIAL
-  dht.begin(); //INICIALIZA A FUNÇÃO 
-   pinMode(amonia,INPUT);   
-  delay(2000); //INTERVALO DE 2 SEGUNDO ANTES DE INICIAR
-}
-void loop(){ 
-    am = analogRead(amonia); 
-       amoniaconvert = map(am,0,1023,0, 290);
-    Serial.print("Umidade: "); //IMPRIME O TEXTO NA SERIAL
-    Serial.print(dht.readHumidity()); //IMPRIME NA SERIAL O VALOR DE UMIDADE MEDIDO
-    Serial.print("%"); //IMPRIME O TEXTO NA SERIAL 
-    Serial.print(" / Temperatura: "); //IMPRIME O TEXTO NA SERIAL
-    Serial.print(dht.readTemperature(), 2); //IMPRIME NA SERIAL O VALOR DE UMIDADE MEDIDO E REMOVE A PARTE DECIMAL
-    Serial.print("*C"); //IMPRIME O TEXTO NA SERIAL 
+int kj;  
+int jk;  
+int xx; 
+int yy;
+DHT dht(DHTPIN, DHTTYPE); 
+int temp=0; 
+ int w =A1;
+int i; 
+int h =13;
+const int CS = 4;  
 
- Serial.print(" amonia PPM convertida "); //IMPRIME O TEXTO NA SERIAL
-    Serial.print(amoniaconvert,2);  
-     Serial.print(" amonia PPM  sem conversao ");
-   Serial.print(am);
-     Serial.println("");
-    delay(60000); //INTERVALO DE 2 SEGUNDOS * NÃO DIMINUIR ESSE VALOR 
+
+void setup()
+{ 
+  pinMode(w,INPUT);
+  Serial.begin(9600);
+  pinMode(h,OUTPUT);
+  if (!SD.begin(CS)) 
+  {
     
+    return;
+  }
+
 }
+
+void loop() 
+{ 
+  dht.begin();
+  
+
+
+  while(i<480 ){// frequencia de repetição
+    File dataFile = SD.open("LEPAV.csv", FILE_WRITE);
+ kj =(dht.readHumidity()); 
+jk =(dht.readTemperature()); 
+xx= analogRead(w);//  y =valor lido convertido para 0 a 5v
+yy =map(xx,0,1024,0,300);
+
+    if (dataFile) 
+    {   
+     
+    dataFile.print("Humidade ");  
+      dataFile.print("; ");  
+      dataFile.print(kj);  
+      dataFile.print("; "); 
+      dataFile.print("Temperatura = ");  
+      dataFile.print("; ");
+      dataFile.print(jk);  
+       dataFile.print("; ");
+      dataFile.print(" Celsius  ");  
+       dataFile.print("; "); 
+       dataFile.print(" PPM  gás  ");  
+       dataFile.print("; "); 
+       dataFile.print(yy);  
+       dataFile.print("; ");
+      dataFile.println(" ");
+      dataFile.close();
+      delay(intervalo); //tempo de leitura em millis
+    //  delay(100); 
+    }
+     i++;}
+    digitalWrite(13,HIGH);
+  } 
